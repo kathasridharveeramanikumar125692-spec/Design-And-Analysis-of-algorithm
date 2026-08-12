0d24db9d1481c170d9248a5ada40044dcf2875b3
Sridhar Katha <katha.sridharveeramanikumar125692@marwadiuniversity.ac.in>
2026-08-12 22:16:44 +0530

    Add files via upload

---
+++ Practical 3/Max Heap Sort.py
@@ -0,0 +1,35 @@
+
+def max_heap(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        max_heap(arr, n, largest)
+
+
+def heap_sort(arr):
+    n = len(arr)
+
+    
+    for i in range(n // 2 - 1, -1, -1):
+        max_heap(arr, n, i)
+
+
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        max_heap(arr, i, 0)
+
+
+arr = list(map(int, input("Enter numbers: ").split()))
+
+heap_sort(arr)
+
+print("Sorted array:", arr)
